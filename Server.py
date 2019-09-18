5e0172ffcc3eff328fc8d828c1476fde0931cf3b
addod0823 <addo.derrick@ymail.com>
2019-09-18 19:40:00 -0400

    Added client and passed players to network by objects

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import socket
 from thread import *
-import sys
+from clue import Player
+import pickle
 
 server = "192.168.1.172"
 port = 5555
@@ -12,40 +13,34 @@ try:
 except socket.error as e:
     str(e)
 
+
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+players = [Player(0,0,10,10,(255,0,0)), Player(100,100,10,10, (0,0,255))] #objects
 
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("Received: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
--- clue.py
+++ clue.py
@@ -1,5 +1,4 @@
 import pygame
-from network import Network
 
 width = 625
 height = 625
@@ -52,93 +51,4 @@ class Player():
 
     def update(self):
 	self.rect = (self.x, self.y, self.width, self.height)
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-
-def redrawWindow(win,player, player2):
-    win.fill((0,0,0))
- 
-    pygame.draw.rect(win, (0,0,0), [0, 0, TILE_WIDTH, TILE_HEIGHT])
-		
-    for row in range(25):
-	for column in range(25):	
-		pygame.draw.rect(win, (255,255,0), [TILE_SIZE*column + TILE_MARGIN, TILE_SIZE*row + TILE_MARGIN,TILE_WIDTH,TILE_HEIGHT])
-	
-
-    #Add Kitchen
-    for row in range(7):
-	for column in range(4):
-    		pygame.draw.rect(win, (BLACK), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+    TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-    #Add lounge
-    for row in range(9,14):
-	for column in range(7):
-    		pygame.draw.rect(win, (BLUE), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+    TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-    #Add Hall
-    for row in range(16,24):
-   	for column in range(6):
-    		pygame.draw.rect(win, (RED), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-    #Add dinning room
-    for row in range(6):
-   	for column in range(6,10):
-    		pygame.draw.rect(win, (GREEN), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-   #Add study
-    for row in range(6):
-   	for column in range(12,16):
-    		pygame.draw.rect(win, (BLUE), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-   #Add Conservatory
-    for row in range(5):
-   	for column in range(18,24):
-    		pygame.draw.rect(win, (RED), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-  #Add Clue
-    for row in range(8, 13):
-   	for column in range(8, 14):
-    		pygame.draw.rect(win, (GREEN), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-  #Add Ballroom
-    for row in range(15,24):
-   	for column in range(8,15):
-    		pygame.draw.rect(win, (BLACK), [TILE_SIZE*row + TILE_MARGIN, TILE_SIZE*column+ TILE_MARGIN,TILE_WIDTH, TILE_HEIGHT])
-
-
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
-
-
-def main():
-    pygame.init()
-    run = True
-    n = Network()
-    startPos = read_pos(n.getPos())
-   
-    p = Player(startPos[0],startPos[1],25,25,(BLUE))
-    p2 = Player(6,6,25,25,(WHITE))
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(90)
-	p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-	p2.x = p2Pos[0]
-	p2.y = p2Pos[1]
-	p2.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.K_ESCAPE:
-            	pygam.quit()
-
-        p.move()
-        redrawWindow(win, p, p2)
-
-main()
 
